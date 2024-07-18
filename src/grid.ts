09661e6ef2c88175a330d26bc6198916e7d1dd93
Tyson <TysonS.417@gmail.com>
2024-07-17 23:39:37 -0700

    grid ts conversion

--- src/grid.ts
+++ src/grid.ts
@@ -2,24 +2,26 @@ import sample from 'lodash/sample';
 import compact from 'lodash/compact';
 import inRange from 'lodash/inRange';
 import fill from 'lodash/fill';
-
+import type Cube from './cube';
 
 export default class Grid {
+  dimensions: [number, number, number];
+  cells: null[] | Cube['position'][];
 
-  constructor(dimensions) {
+  constructor(dimensions: Grid['dimensions']) {
     this.dimensions = dimensions;
     this.cells = Grid.buildFlatArray(dimensions);
   }
 
   // build a 1D array given 3D dimensions
-  static buildFlatArray(dimensions) {
+  static buildFlatArray(dimensions: Grid['dimensions']) {
     const [width, height, depth] = dimensions;
     return fill(Array(width * height * depth), null)
   };
 
-  isCellAvailable = (cell) => !this.isCellOccupied(cell);
+  isCellAvailable = (cell: Cube['position']) => !this.isCellOccupied(cell);
 
-  isCellOccupied = (cell) => !!this.getCellContent(cell);
+  isCellOccupied = (cell: Cube['position']) => !!this.getCellContent(cell);
 
   hasAvailableCells = () => !!this.findAvailableCells().length;;
 
@@ -27,17 +29,17 @@ export default class Grid {
 
   findRandomAvailableCell = () => sample(this.findAvailableCells());
 
-  getCellContent = (cell) => this.isWithinBounds(cell) && this.cells[this.positionToIndex(cell)];
+  getCellContent = (cell: Cube['position']) => this.isWithinBounds(cell) && this.cells[this.positionToIndex(cell)];
 
   findAvailableCells = () => {
-    const cells = [];
+    const cells: Exclude<Grid['cells'], null[]> = [];
     this.forEachCell((x, y, z, cube) => {
       !cube && cells.push({ x, y, z });
     });
     return cells;
   };
 
-  indexToPosition = (position) => {
+  indexToPosition = (position: number) => {
     const [width, height, depth] = this.dimensions;
     const doubleSize = width * height;
     const x = position % width;
@@ -47,13 +49,13 @@ export default class Grid {
     return { x, y, z };
   };
 
-  positionToIndex = ({ x, y, z }) => {
+  positionToIndex = ({ x, y, z }: Cube['position']) => {
     // TODO: Validate depth usage
     const [width, height, depth] = this.dimensions;
     return x + (y * height) + (z * width * height);
   };
 
-  forEachCell = (cb) => {
+  forEachCell = (cb: (x: number, y: number, z: number, cell: Cube['position'] | null) => void) => {
     this.cells.forEach((cell, position) => {
       const { x, y, z } = this.indexToPosition(position);
       cb(x, y, z, cell);
@@ -61,19 +63,19 @@ export default class Grid {
     return this;
   };
 
-  insert = (cube) => {
+  insert = (cube: Cube) => {
     const position = this.positionToIndex(cube);
     this.cells[position] = cube;
     return this;
   };
 
-  remove = (cube) => {
+  remove = (cube: Cube) => {
     const position = this.positionToIndex(cube);
     this.cells[position] = null;
     return this;
   };
 
-  isWithinBounds = (position) => {
+  isWithinBounds = (position: Cube['position']) => {
     const { x, y, z } = position;
     const [width, height, depth] = this.dimensions;
     return inRange(x, 0, width) && inRange(y, 0, height) && inRange(z, 0, depth);
